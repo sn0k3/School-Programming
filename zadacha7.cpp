88a2c0452aa6b4553add005b64ff443601ad45b2
sn0k3 <ih999@abv.bg>
2014-03-10 10:31:01 +0200

    Create zadacha7.cpp

---
+++ zadacha7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// Да се изведът всички цели числа от 5 до 100, които се делят на 5
+
+int main()
+{
+
+  for(int i=5;i<=100;i=i+5)
+  {
+      cout << i << endl;
+  }
+
+  return 0;
+}
